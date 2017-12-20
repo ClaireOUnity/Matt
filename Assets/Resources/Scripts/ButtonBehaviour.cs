d03c438806b1c0af7633b3130eae911e5dbe655b
Unknown <matt_cooper1@hotmail.com>
2017-12-20 02:40:57 -0800

    Added button behaviour and nextwave class

--- Assets/Resources/Scripts/ButtonBehaviour.cs
+++ Assets/Resources/Scripts/ButtonBehaviour.cs
@@ -2,20 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour {
-
-    GameObject menuPanel;
-
+public class ButtonBehaviour : MonoBehaviour
+{
     void Start()
     {
-        menuPanel = GameObject.Find("MenuPanel");
-    }
 
-    public void ToggleMenuPanel()
+    }
+    private void Update()
     {
-        if (menuPanel.activeInHierarchy)
-            menuPanel.SetActive(false);
-        else menuPanel.SetActive(true);
+        foreach (Transform child in this.transform)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                child.gameObject.SetActive(false);
+            }
+            else if (Input.GetKey(KeyCode.M))
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+    
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+    public void ResetScene()
+    {
+        Application.LoadLevel("Map1");
     }
 }
 
