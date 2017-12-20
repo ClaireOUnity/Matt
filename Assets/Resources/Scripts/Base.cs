953a3e9cc5af176afd3eadfa4d9470601294b57d
Unknown <matt_cooper1@hotmail.com>
2017-12-20 02:41:16 -0800

    added end game - when bases health is 0 game will reset

---
+++ Assets/Resources/Scripts/Base.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour {
+
+    int health;
+
+	// Use this for initialization
+	void Start () {
+        health = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0)
+            Application.LoadLevel("Map1");
+	}
+}
